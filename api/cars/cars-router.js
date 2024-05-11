7732a9f6c2f16f4ac0c1792eb25b3692202f9318
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-11 08:06:01 -0700

    checkID router completed

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,14 +1,22 @@
 const express = require('express')
-
+const Car = require('./cars-model')
 const router = express.Router()
+const {
+    checkCarId
+} = require('./cars-middleware')
 
 
 router.get('/', async (req, res, next) => {
-    res.json('getting all cars')
+    try {
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch (err) {
+        next(err)
+    }
 })
 
-router.get('/:id', async (req, res, next) => {
-    res.json(`gettin cars with id ${req.params.id}`)
+router.get('/:id',checkCarId, async (req, res, next) => {
+      res.json(req.car)
 })
 
 router.post('/', async (req, res, next) => {
