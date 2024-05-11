321ef510837e824966a889c5d4dc3d2b5e6cb257
Marijana Taseska <marijana.taseska@gmail.com>
2024-05-10 22:48:20 -0700

    server.js completed and working

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,36 @@
 // STRETCH
+const cars =[
+    {
+        vin: '1111111111111',
+        make:'toyota',
+        model: 'prius',
+        mileage:215000,
+        title:'clean',
+        transmission: 'manual',
+    },
+    {
+        vin: '2222222222222',
+        make:'toyota',
+        model: 'corola',
+        mileage:115000,
+        title:'salvage',
+    },
+    {
+        vin: '33333333333333',
+        make:'ford',
+        model: 'focus',
+        mileage:135000,
+    },
+]
+
+// exports.seed = function(knex) {
+//     return knex('cars')
+//     .truncate().then(() => {
+//         return knex('cars').insert(cars)
+//     })
+// }
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
